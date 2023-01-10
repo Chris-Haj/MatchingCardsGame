b72d28e5057d4ef5b9149fcbb473d4053d6940c7
Chris-Haj <chris1191@gmail.com>
2023-01-10 04:33:12 +0200

    Cards flip now. Yay.

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// const OneCards = document.querySelectorAll('.OneCard');
+//
+// for (let i = 0; i < OneCards.length; i++) {
+//     OneCards[i].style.gridRow = Math.floor(i / 5) + 1;
+//     OneCards[i].style.gridColumn = (i % 5) + 1;
+// }
